64c564b4c0a90442f1ba7b2109998f95a660671e
deawiw <madina.korish@gmail.com>
2026-05-16 22:55:18 +0300

    Update source path for mcp_server.R

--- mcp_scheduler.R
+++ mcp_scheduler.R
@@ -1,7 +1,7 @@
 # mcp_scheduler.R
 # Автоматический анализ всех непроверенных статей
 
-source("mcp_server.R")
+source("mcp_server/mcp_server.R")
 
 # Обработать все статьи (лимит 10000 = все)
 process_unchecked_articles(limit = 10000)
